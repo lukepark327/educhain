03b8925dcd85ec8f8a92f52e3056c9999b2fc24c
twodude <twodude@naver.com>
2019-01-21 05:32:14 +0900

    move

--- agent/main.py
+++ agent/main.py
@@ -1,20 +1,4 @@
+import actions
+
 if __name__ == '__main__':
-    tests = []
-
-    # import test cases
-    from test1 import main as test1
-    tests.append(test1)
-
-    """
-    Run test(s)
-    """
-    pass_cnt = 0
-    total_cnt = 0
-
-    for test in tests:
-        pass_cnt_per_test, total_cnt_per_test = test.scenario()
-        pass_cnt += pass_cnt_per_test
-        total_cnt += total_cnt_per_test
-
-    # print test results
-    print(str(total_cnt - pass_cnt) + " of " + str(total_cnt) + " tests failed.")
+    pass
